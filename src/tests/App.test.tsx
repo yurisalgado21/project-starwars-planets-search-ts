f7436c676be2f81e4300b4d766362496386be711
Yuri Salgado Farias Torres <yuri.salgado.ft@gmail.com>
2023-09-15 07:33:00 -0300

    Exercise 5 done

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from '../App';
+import DataProvider from '../context/DataProvider';
 
 test('I am your test', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Hello, App!/i);
-  expect(linkElement).toBeInTheDocument();
+  render(
+  <DataProvider>
+    <App />
+  </DataProvider>);
+  expect(screen.getByRole('textbox')).toBeInTheDocument();
+  expect(screen.getByText(/operador:/i)).toBeInTheDocument();
+  expect(screen.getByText(/coluna:/i)).toBeInTheDocument();
 });
