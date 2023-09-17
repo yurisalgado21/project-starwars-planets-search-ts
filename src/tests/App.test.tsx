f43442f204271317103adfbf8a5e8e739ca518d2
Yuri Salgado Farias Torres <yuri.salgado.ft@gmail.com>
2023-09-16 23:59:10 -0300

    Exercise 8 done

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -2,13 +2,21 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from '../App';
 import DataProvider from '../context/DataProvider';
+import userEvent from '@testing-library/user-event';
 
 test('I am your test', () => {
   render(
   <DataProvider>
     <App />
   </DataProvider>);
-  expect(screen.getByRole('textbox')).toBeInTheDocument();
-  expect(screen.getByText(/operador:/i)).toBeInTheDocument();
-  expect(screen.getByText(/coluna:/i)).toBeInTheDocument();
+  const input = screen.getByRole('textbox');
+  const spinbutton = screen.getByRole('spinbutton');
+  const buttonFilter = screen.getByRole('button', {
+    name: /filtrar/i
+  })
+  expect(input).toBeInTheDocument();
+  expect(spinbutton).toBeInTheDocument();
+  expect(buttonFilter).toBeInTheDocument();
+  userEvent.type(input, 'tato');
+  userEvent.type(spinbutton, '10000');
 });
