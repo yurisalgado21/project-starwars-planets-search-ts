4a58b437e8097345c2842877223e19be59079fca
Yuri Salgado Farias Torres <yuri.salgado.ft@gmail.com>
2023-09-19 10:54:11 -0300

    Exercise 8 done

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -14,9 +14,20 @@ test('I am your test', () => {
   const buttonFilter = screen.getByRole('button', {
     name: /filtrar/i
   })
+  const inputRadioASC = screen.getByText(/ascendente/i);
+  const inputRadioDESC = screen.getByText(/descendente/i);
+  const buttonOrder = screen.getByRole('button', {
+    name: /ordenar/i
+  });
   expect(input).toBeInTheDocument();
   expect(spinbutton).toBeInTheDocument();
   expect(buttonFilter).toBeInTheDocument();
+  expect(inputRadioASC).toBeInTheDocument();
+  expect(inputRadioDESC).toBeInTheDocument();
+  expect(buttonOrder).toBeInTheDocument();
   userEvent.type(input, 'tato');
   userEvent.type(spinbutton, '10000');
+  userEvent.click(buttonFilter);
+  userEvent.click(inputRadioASC);
+  userEvent.click(buttonOrder);
 });
